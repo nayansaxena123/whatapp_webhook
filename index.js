4ab246334345379becaf323f97bec0ee8d865cde
nayansaxena123 <30698927+nayansaxena123@users.noreply.github.com>
2024-01-08 16:50:48 +0530

    hj

--- index.js
+++ index.js
@@ -6,26 +6,38 @@ const port = 8800;
 
 
 app.get("/whatsapp_webhook", (req, res) => {
-    let mode=req.query["hub.mode"];
-    let challange=req.query["hub.challenge"];
-    let token=req.query["hub.verify_token"];
+    // let mode=req.query["hub.mode"];
+    // let challange=req.query["hub.challenge"];
+    // let token=req.query["hub.verify_token"];
+    console.log('test')
 
-    const mytoken="12345@quadra"
+  
+      if (
+        req.query['hub.mode'] == 'subscribe' &&
+        req.query['hub.verify_token'] == '12345@quadra'
+      ) {
+        res.send(req.query['hub.challenge']);
+      } else {
+        res.sendStatus(400);
+      }
+    
 
-    if(mode=="subscribe" && token=="12345@quadra"){
-        res.status(200).send(challange);
-        console.log(JSON.stringify(req.body,'response'));
-    }else{
-        res.status(403);
-    }
+    // const mytoken="12345@quadra"
+
+    // if(mode=="subscribe" && token=="12345@quadra"){
+    //     res.status(200).send(challange);
+    //     console.log(JSON.stringify(req.body,'response'));
+    // }else{
+    //     res.status(403);
+    // }
   res.send("Hello World!");
 });
 
 
 app.post('/whatsapp_webhook', (req, res) => {
 
-    let body_params=req.body
-    console.log(JSON.stringify(body_params,null,2)) // print all response
+    // let body_params=req.body
+    console.log(req.body,'req') // print all response
 
     // if(body_params.object){
     //     if(body_params.entry &&
@@ -44,7 +56,7 @@ app.post('/whatsapp_webhook', (req, res) => {
   
     //messageFrom=req.body['data']['from'] // sender number
     //messageMsg=req.body['data']['body'] // Message text
-    // res.status(200).end()
+    res.status(200);
   })
   
 
