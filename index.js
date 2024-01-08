8deeeea151a7184f5338b6d53699170839c086f2
nayansaxena123 <30698927+nayansaxena123@users.noreply.github.com>
2024-01-08 16:26:36 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/whatsapp_webhook", (req, res) => {
 
     if(mode==="subscribe" && token==="12345@quadra"){
         res.status(200).send(challange);
-        console.log(JSON.stringify(req.body));
+        console.log(JSON.stringify(req.body,'response'));
     }else{
         res.status(403);
     }
@@ -22,30 +22,30 @@ app.get("/whatsapp_webhook", (req, res) => {
 });
 
 
-app.post('/whatsapp_webhook', (req, res) => {
+// app.post('/whatsapp_webhook', (req, res) => {
 
-    let body_params=req.body
-    console.log(JSON.stringify(body_params,null,2)) // print all response
+//     let body_params=req.body
+//     console.log(JSON.stringify(body_params,null,2)) // print all response
 
-    if(body_params.object){
-        if(body_params.entry &&
-             body_params.entry[0].changes && 
-             body_params.entry[0].changes[0].value.messages &&
-             body_params.entry[0].changes[0].value.messages[0] 
-             ){
-                let phn_no_id= body_params.entry[0].changes[0].value.metadata.phone_number_id;
-                let from=body_params.entry[0].changes[0].value.messages[0].from;
-                let msg_body=body_params.entry[0].changes[0].value.messages[0].text.body;
-                console.group(phn_no_id,from,msg_body)
+//     if(body_params.object){
+//         if(body_params.entry &&
+//              body_params.entry[0].changes && 
+//              body_params.entry[0].changes[0].value.messages &&
+//              body_params.entry[0].changes[0].value.messages[0] 
+//              ){
+//                 let phn_no_id= body_params.entry[0].changes[0].value.metadata.phone_number_id;
+//                 let from=body_params.entry[0].changes[0].value.messages[0].from;
+//                 let msg_body=body_params.entry[0].changes[0].value.messages[0].text.body;
+//                 console.group(phn_no_id,from,msg_body)
 
-             }
+//              }
         
-    }
+//     }
   
-    //messageFrom=req.body['data']['from'] // sender number
-    //messageMsg=req.body['data']['body'] // Message text
-    res.status(200).end()
-  })
+//     //messageFrom=req.body['data']['from'] // sender number
+//     //messageMsg=req.body['data']['body'] // Message text
+//     res.status(200).end()
+//   })
   
 
 app.listen(port, () => {
