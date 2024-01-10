9f0130c8a02205306739f9b7e38423a4dd8af118
nayansaxena123 <30698927+nayansaxena123@users.noreply.github.com>
2024-01-10 15:22:44 +0530

    syntex error fixed

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ app.post('/whatsapp_webhook', (req, res) => {
     //messageFrom=req.body['data']['from'] // sender number
     //messageMsg=req.body['data']['body'] // Message text
     let msg_body=req.body.entry[0].changes[0].value.messages[0].text.body;
-    let from=body_params.entry[0].changes[0].value.messages[0].from;
+    let from=req.body.entry[0].changes[0].value.messages[0].from;
     console.log('request header X-Hub-Signature validated',msg_body,from);
    
   // Process the Facebook updates here
