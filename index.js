139f439b364784cd52a3beefc39bb68124b17c3e
nayansaxena123 <30698927+nayansaxena123@users.noreply.github.com>
2024-01-24 01:15:52 +0530

    name

--- index.js
+++ index.js
@@ -158,7 +158,8 @@ app.post('/whatsapp_webhook', (req, res) => {
     try {
       let msg_body = req.body.entry[0]?.changes[0]?.value.messages[0]?.text.body;
     let from = req.body.entry[0]?.changes[0]?.value.messages[0]?.from;
-    console.log('request header X-Hub-Signature validated', msg_body, from);
+    let name=req.body.entry[0]?.changes[0]?.contacts.profile[0]?.name;
+    console.log('request header X-Hub-Signature validated', msg_body, from,name);
     sendersnum = from;
     sendersMsg = msg_body;
     console.log(sendersMsg, sendersnum, 'cus-naming')
